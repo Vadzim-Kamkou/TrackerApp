3b38380db879158a0f5919c808871656cb928a27
VadzimKamkou <vadim.komkov@vandrouka.games>
2025-06-13 13:59:08 +0300

    Initial Commit

---
+++ TrackerTests/TrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  TrackerTests.swift
+//  TrackerTests
+//
+//  Created by Vadzim on 13.06.25.
+//
+
+import Testing
+@testable import Tracker
+
+struct TrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
