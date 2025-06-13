dcf0a76bed22fc01d003be8c274171c99108e853
Vadzim-Kamkou <vadzim.kamkou@gmail.com>
2025-06-13 15:43:20 +0300

    Test

--- Tracker/ViewController.swift
+++ Tracker/ViewController.swift
@@ -11,7 +11,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("main")
+        print("main test")
         // Do any additional setup after loading the view.
     }
 
