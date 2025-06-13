375bc4436b6873af928c62fd7b20cbf2ac93218c
VadzimKamkou <vadim.komkov@vandrouka.games>
2025-06-13 14:49:51 +0300

    Initial commit

--- Tracker/ViewController.swift
+++ Tracker/ViewController.swift
@@ -11,6 +11,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("main")
         // Do any additional setup after loading the view.
     }
 
